d59d32f93fe281383aa8bf8ddebd6e12b51c417a
Lizeth Rodriguez <101153941+lizeth45@users.noreply.github.com>
2022-11-24 22:03:01 -0600

    Delete API-Unacho-main directory

--- API-Unacho-main/ProyectoUnacho/src/main/java/com/app/proyecto/jpa/IUsuariojpa.java
+++
@@ -1,9 +0,0 @@
-package com.app.proyecto.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.proyecto.entity.Usuario;
-
-public interface IUsuariojpa extends JpaRepository<Usuario, Integer>{
-
-}
