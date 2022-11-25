c20245dabc86421e82c61386bafcfefa32dd81b1
Lizeth Rodriguez <101153941+lizeth45@users.noreply.github.com>
2022-11-24 22:05:46 -0600

    Delete API-Unacho-main directory

--- API-Unacho-main/ProyectoUnacho/src/main/java/com/app/proyecto/jpa/IPlaylistjpa.java
+++
@@ -1,9 +0,0 @@
-package com.app.proyecto.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.proyecto.entity.Playlist;
-
-public interface IPlaylistjpa extends JpaRepository<Playlist, Integer>{
-
-}
