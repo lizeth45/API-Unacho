9c4da79bbef390717c1b280dae29e3b15ced9e47
Lizeth Rodriguez <101153941+lizeth45@users.noreply.github.com>
2022-11-24 22:05:01 -0600

    Delete ProyectoUnacho directory

--- ProyectoUnacho/src/main/java/com/app/proyecto/jpa/ICancionesjpa.java
+++
@@ -1,9 +0,0 @@
-package com.app.proyecto.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.proyecto.entity.Canciones;
-
-public interface ICancionesjpa extends JpaRepository<Canciones, Integer>{
-
-}
